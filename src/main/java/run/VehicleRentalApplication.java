6285310f584fdecab99cb590618149889411ad85
natalie <nataliesandford@hotmail.co.uk>
2018-02-09 17:18:51 +0000

    Removed unused code.

--- src/main/java/run/VehicleRentalApplication.java
+++ src/main/java/run/VehicleRentalApplication.java
@@ -3,7 +3,7 @@ package run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"controllers"})// same as @Configuration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication(scanBasePackages={"controllers"})
 public class VehicleRentalApplication {
 
     public static void main(String[] args) {
